bdfe4e6094888980cb06621c6a897faaf066cd4f
Cromefire_ <cromefire_@outlook.com>
2022-12-17 20:52:09 +0100

    Fixed technology effects

--- prototypes/technologies.lua
+++ prototypes/technologies.lua
@@ -1,80 +1,80 @@
 --Basic Robots
-table.insert(data.raw["technology"]["logistic-robotics"], {
+table.insert(data.raw["technology"]["logistic-robotics"].effects, {
     type = "unlock-recipe",
     recipe = "logistic-robot"
 })
 
-table.insert(data.raw["technology"]["construction-robotics"], {
+table.insert(data.raw["technology"]["construction-robotics"].effects, {
     type = "unlock-recipe",
     recipe = "construction-robot"
 })
 
 --Ralesia Platation
-table.insert(data.raw["technology"]["machines-mk02"], {
+table.insert(data.raw["technology"]["machines-mk02"].effects, {
     type = "unlock-recipe",
     recipe = "ralesia-plantation-mk02"
 })
 
-table.insert(data.raw["technology"]["machines-mk03"], {
+table.insert(data.raw["technology"]["machines-mk03"].effects, {
     type = "unlock-recipe",
     recipe = "ralesia-plantation-mk03"
 })
 
-table.insert(data.raw["technology"]["machines-mk04"], {
+table.insert(data.raw["technology"]["machines-mk04"].effects, {
     type = "unlock-recipe",
     recipe = "ralesia-plantation-mk04"
 })
 
 --Cadaveric Arum
-table.insert(data.raw["technology"]["advanced-electronics"], {
+table.insert(data.raw["technology"]["advanced-electronics"].effects, {
     type = "unlock-recipe",
     recipe = "cadaveric-arum-mk02"
 })
 
-table.insert(data.raw["technology"]["graphene"], {
+table.insert(data.raw["technology"]["graphene"].effects, {
     type = "unlock-recipe",
     recipe = "cadaveric-arum-mk03"
 })
 
-table.insert(data.raw["technology"]["nano-tech"], {
+table.insert(data.raw["technology"]["nano-tech"].effects, {
     type = "unlock-recipe",
     recipe = "cadaveric-arum-mk04"
 })
 
 --Fawogae Plantation
-table.insert(data.raw["technology"]["machines-mk02"], {
+table.insert(data.raw["technology"]["machines-mk02"].effects, {
     type = "unlock-recipe",
     recipe = "fawogae-plantation-mk02"
 })
 
-table.insert(data.raw["technology"]["machines-mk03"], {
+table.insert(data.raw["technology"]["machines-mk03"].effects, {
     type = "unlock-recipe",
     recipe = "fawogae-plantation-mk03"
 })
 
-table.insert(data.raw["technology"]["machines-mk04"], {
+table.insert(data.raw["technology"]["machines-mk04"].effects, {
     type = "unlock-recipe",
     recipe = "fawogae-plantation-mk04"
 })
 
 --Moondrop Greenhouse
-table.insert(data.raw["technology"]["machines-mk02"], {
+table.insert(data.raw["technology"]["machines-mk02"].effects, {
     type = "unlock-recipe",
     recipe = "moondrop-greenhouse-mk02"
 })
 
-table.insert(data.raw["technology"]["machines-mk03"], {
+table.insert(data.raw["technology"]["machines-mk03"].effects, {
     type = "unlock-recipe",
     recipe = "moondrop-greenhouse-mk03"
 })
 
-table.insert(data.raw["technology"]["machines-mk04"], {
+table.insert(data.raw["technology"]["machines-mk04"].effects, {
     type = "unlock-recipe",
     recipe = "moondrop-greenhouse-mk04"
 })
 
 --Nitrobenzene
-table.insert(data.raw["technology"]["fine-electronics"], {
+table.insert(data.raw["technology"]["fine-electronics"].effects, {
     type = "unlock-recipe",
     recipe = "nitrobenzene"
 })
